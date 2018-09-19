345252977a9893d354b08072699fed561c3fd024
omar-nav <omar.navarro@g.austincc.edu>
2018-09-18 23:25:16 -0500

    zxcvbn library determines password complexity

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,24 @@
 const router = require('express').Router()
 const User = require('../models/User')
 const passport = require('passport')
+const zxcvbn = require('zxcvbn')
 
-
-// agregar usuario
+// add user
 router.get('/addUser', (req, res, next) => {
   res.render('auth/addUser')
 })
 router.post('/addUser', (req, res, next) => {
+  const { password, password2 } = req.body
+
+  if (password !== password2) return res.render('auth/addUser', { error: 'Confirma que las contraseñas son iguales' })
+  if (zxcvbn(password).score <= 1) return res.render('auth/addUser', zxcvbn(password).feedback)
+
   User.register(req.body, req.body.password)
     .then(user => res.redirect('/list'))
     .catch(error => next(error))
 })
 
-// iniciar sesion
+// login
 router.get('/login', (req, res, next) => {
   res.render('auth/login')
 })
@@ -21,7 +26,7 @@ router.post('/login', passport.authenticate('local'), (req, res, next) => {
   res.redirect('/list')
 })
 
-// listado de usuarios
+// list users
 router.get('/list', (req, res, next) => {
   User.find().then(users => {
     res.render('auth/list', { users })
